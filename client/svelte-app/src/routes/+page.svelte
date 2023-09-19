6ae847b1683501fe2acc2043c50b73a44dd7b6f7
rapythereaper <abhiyanbhurtel@gmail.com>
2023-09-19 19:55:50 +0545

    added design to home page

--- client/svelte-app/src/routes/+page.svelte
+++ client/svelte-app/src/routes/+page.svelte
@@ -1,2 +1,46 @@
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+              
+              <div class="avatar">
+    <div class="w-44 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
+      <img src="./logo.jpg" alt="logo"/>
+    </div>
+  </div>
+              
+        <h1 class="text-5xl font-bold">Coming Soon....</h1>
+        <p class="py-6">LIVE Event </p>
+        <a href="#event-0">
+                  <button class="btn btn-accent btn-circle bounce2">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"/></svg></button>
+                  </a>
+              
+      </div>
+    </div>
+  </div>
+  
+  
+  <div id= "event-0" class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row">
+           <div>
+        <h1 class="text-5xl font-bold gradient-text">ACIT Design Blitz-2023!</h1>
+        <p class="py-6">ACIT Design Blitz-2023 is held with the slogan "60 min of Graphical Brilliance". It is an apportunity for design enthusiasts to unlock their potential</p>
+                 <div class="text-center">
+              <a href="/DesignComp"> <button class="btn btn-secondary">Go to ACIT Design Blitz</button> </a>
+                           </div>
+      </div>
+      <img src="./designComp.jpg" class="max-w-sm rounded-lg shadow-2xl" alt="DesignBlitz Poster" />
+    </div>
+  </div>
 
-Acit
+
+<style>
+.bounce2 {
+  animation: bounce2 2s ease infinite;
+}
+@keyframes bounce2 {
+	0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
+	40% {transform: translateY(-30px);}
+	60% {transform: translateY(-15px);}
+}
+</style>

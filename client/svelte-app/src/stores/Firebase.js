107b58ee400e7909141257c0b2a83e2ad1e9bcb3
rapythereaper <abhiyanbhurtel@gmail.com>
2023-09-20 17:35:19 +0545

    added production & developement build check

--- client/svelte-app/src/stores/Firebase.js
+++ client/svelte-app/src/stores/Firebase.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
     appId: "1:545106967277:web:087f5050607b25a73e2ae4"
   };  
 
-const inProduction=false;
 
 const app = initializeApp(firebaseConfig);
 
@@ -25,15 +24,25 @@ export const FIREBASE_FIRESTORE=getFirestore();
 export const FIREBASE_STORAGE=getStorage();
 
 
+
+
 import {connectAuthEmulator } from "firebase/auth";
 import { connectFunctionsEmulator } from 'firebase/functions';
 import {connectFirestoreEmulator} from "firebase/firestore"
 import {connectStorageEmulator} from "firebase/storage";
-if(!inProduction){
+
+
+/*Added the console message inside if to verify that indeed the controoll flow went inside the if  blocks*/
+if(process.env.NODE_ENV=="development"){
+  console.log(`[*] Running in ${process.env.NODE_ENV} mode.`);
+
   connectAuthEmulator(FIREBASE_AUTH, "http://localhost:9099");
   connectFunctionsEmulator(FIREBASE_FUNCTION,"localhost",5001);
   connectFirestoreEmulator(FIREBASE_FIRESTORE,"localhost",8080);
   connectStorageEmulator(FIREBASE_STORAGE,"localhost",9199);
+}else{
+  console.log(`[*] Running in ${process.env.NODE_ENV} mode.`);
+
 }
 
 export const generateRandomToken=()=>{
